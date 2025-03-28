12b4124b55c9f14eb0a469bad82823c484f8694b
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-03-28 14:29:08 -0300

    created wishListController and wishController

---
+++ src/main/java/controller/WishController.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class WishController {
+//	✅ Manipulam desejos:
+//
+//		markAsCompleted() – Marcar um desejo como concluído.
+//
+//		editWish() – Editar os detalhes de um desejo (nome, descrição, preço).
+//
+//		removeWish() – Remover um desejo de uma lista.
+//
+//	✅ Validações específicas:
+//
+//		addWish() – Deve verificar se:
+//
+//		O desejo já existe na lista (lançar WishExtantException).
+//
+//		O desejo pertence ao usuário correto (lançar WishInUseException).
+}
