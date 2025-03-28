05405a38c4e2dfb1ffa4a943002ecf116449c40a
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-03-28 14:35:14 -0300

    created connectionException

---
+++ src/main/java/exceptions/ConnectionException.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+public class ConnectionException extends Exception {
+	
+	public ConnectionException(String message) {
+		super(message);
+	}
+	
+	public ConnectionException(Throwable cause) {
+		super(cause);
+	}
+	
+	public ConnectionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
