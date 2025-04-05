15f87cf3d9d490b99d2adb811af9a0474e071ce1
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-04-05 16:20:40 -0300

    desenvolvido o login

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -3,6 +3,9 @@ package view;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,6 +17,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.border.EmptyBorder;
+import controller.UserController;
+import dao.UserDao;
 
 public class LoginView extends JFrame {
 
@@ -26,7 +31,7 @@ public class LoginView extends JFrame {
         setTitle("Lista de compras - Login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 900, 500);
-        setLocationRelativeTo(null); // Centraliza a janela na tela
+        setLocationRelativeTo(null);
         
         login = new JPanel();
         login.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -53,6 +58,15 @@ public class LoginView extends JFrame {
         passwordField = new JPasswordField();
         passwordField.setFont(new Font("Tahoma", Font.PLAIN, 14));
         passwordField.setColumns(20);
+        
+        JButton backButton = new JButton("Voltar");
+        backButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	LoginView.this.dispose(); // Fecha a tela de login
+			    new HomeView().setVisible(true);
+            }
+        });
 
         JButton loginButton = new JButton("Entrar");
         loginButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -79,8 +93,26 @@ public class LoginView extends JFrame {
                     return;
                 }
                 
+                UserDao userDao = new UserDao();
+                UserController userController = new UserController(userDao);
+                
+                try {
+					if (userController.loginUserController(email, password)) {
+						LoginView.this.dispose(); // Fecha a tela de login
+					    new UserView(userController).setVisible(true); // Abre a tela do user
+					} else {
+	                    JOptionPane.showMessageDialog(LoginView.this,
+	                            "Senha ou email incorreto!", "Erro", JOptionPane.ERROR_MESSAGE);
+	                    return;
+					}
+				} catch (NoSuchAlgorithmException | InvalidKeySpecException e1) {
+					System.out.println("Erro ao verificar email e senha: " + e1.getMessage());
+				}
+                
                 emailField.setText("");
                 passwordField.setText("");
+                
+                
             }
         });
 
@@ -98,7 +130,8 @@ public class LoginView extends JFrame {
                         .addGroup(gl_login.createParallelGroup(Alignment.LEADING)
                             .addComponent(emailField, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)
                             .addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(loginButton, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+                    .addComponent(loginButton, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(backButton, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(200, Short.MAX_VALUE)
         );
 
@@ -116,6 +149,8 @@ public class LoginView extends JFrame {
                     .addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                 .addGap(30)
                 .addComponent(loginButton)
+                .addGap(40)
+                .addComponent(backButton)
                 .addContainerGap(80, Short.MAX_VALUE)
         );
     }
